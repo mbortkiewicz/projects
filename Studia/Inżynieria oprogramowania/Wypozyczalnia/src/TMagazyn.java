83469ebe056f2217dc7d4651b7ee07930595aa93
Mateusz B <mateusz.bortkiewicz@gmail.com>
2017-11-24 13:34:32 +0100

    Dodanie kodu Wypozyczalni, poprawa diagramu klas

---
+++ Studia/Inżynieria oprogramowania/Wypozyczalnia/src/TMagazyn.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class TMagazyn {
+
+	private List<TProdukt> produkty;
+	private List<TTytul> tytuly;
+	
+	public TProdukt szukaj_TProdukt(TProdukt produkt)
+	{
+		return null;
+	}
+	public TTytul szukaj_TTytul(TTytul tytul)
+	{
+		return null;
+	}
+	public String dodaj_TProdukt(String[] data)
+	{
+		return null;
+	}
+	public String dodaj_TTytul(String[] data)
+	{
+		return null;
+	}
+	public void drukuj_TProdukty()
+	{
+		return;
+	}
+	public void drukuj_TTytuly()
+	{
+		return;
+	}
+}
